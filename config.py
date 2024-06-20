0470211e7a7bd825668d3563be2c0bd346c00b15
rider143k <150319044+rider143k@users.noreply.github.com>
2024-06-21 00:31:30 +0530

    Update config.py
    Dd

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6036448711"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://rider143k:rajkumar1234@@@cluster0.naozi3l.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://aniket:rkraja@cluster0.6nd8hqg.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Shortner (token system) 
